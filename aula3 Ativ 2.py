674389cdda08ce642be72654f7d05ac6dd6770b1
Ronaldo <rpetronilo@hotmail.com>
2025-03-22 12:30:24 -0300

    Add files via upload

---
+++ aula3 Ativ 2.py
@@ -0,0 +1,9 @@
+N1 = float( input("digite um número"))
+N2 = float( input("digite um número"))
+N3 = float( input("digite um número"))
+N4 = float( input("digite um número"))
+
+print ("SOMA", N1 + N2 + N3 + N4) 
+print ("SUBTRAÇÃO", N1 / N2 + N3 - N4)
+print ("MULTIPLICAÇÃO", (N1 + N2 + N3 - N4)*5)
+print ("MÉDIA", ((N1 + N2 + N3 + N4)/4))
