acdc14abec50c189bc23688c1b7b7712a371b85c
Ronaldo <rpetronilo@hotmail.com>
2025-03-22 17:52:21 -0300

    Add files via upload

---
+++ novo.py
@@ -0,0 +1,43 @@
+# 1 -Imprima uma mensagem de boas-vindas na tela.
+print('Sejam Bem Vindos')
+# 2 - Declare uma variável booleana verdadeiro 
+# com o valor True e imprima seu tipo
+t = True
+tipo = type(t)
+print(tipo)
+
+# 3 - Imprima o resultado da multiplicação de dois números 
+# decimais de sua escolha
+resultado1 = float(2.14)
+resultado2 = float(7.14)
+total = (resultado1*resultado2)
+print(total)
+
+# 4 - Imprima o resultado da divisão de dois números inteiros 
+# de sua escolha.
+resultado1 = int(14)
+resultado2 = int(7)
+total = (resultado1/resultado2)
+print(total)
+
+# 5 - Imprima o resultado da subtração de dois números 
+# inteiros de sua escolha
+resultado1 = int(14)
+resultado2 = int(7)
+total = (resultado1 - resultado2)
+print(total)
+
+# 6 - Imprima o resultado da divisão (//) inteira de dois números 
+# inteiros de sua escolha.
+resultado1 = int(14)
+resultado2 = int(14)
+total = (resultado1//resultado2)
+print(total)
+
+# 7 - Imprima o resultado da multiplicação de 4 números decimais de sua escolha
+resultado1 = float(2.14)
+resultado2 = float(7.14)
+resultado3 = float(0.14)
+resultado4 = float(25.14)
+multiplicacao = (resultado1*resultado2*resultado3*resultado4)
+print(multiplicacao)
