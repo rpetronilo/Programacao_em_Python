beb94c83fc4f4b45b9b2ef53cc3e2b91c271e203
Ronaldo <rpetronilo@hotmail.com>
2025-03-29 12:38:46 -0300

    Add files via upload

---
+++ Exercicio 6.1.py
@@ -0,0 +1,25 @@
+resultado = (4 * 6) / 2
+print(resultado)
+
+resultado = 5 + 7
+print(resultado)
+
+resultado1 = (4 * 6) / 2
+
+# Elevar 3 à potência de 4
+resultado2 = 3 ** 4
+print(resultado1, resultado2)
+
+# Calcular a média de 8, 12 e 15
+media = (8 + 12 + 15) / 3
+
+# Subtrair 10 de 2 e multiplicar o resultado por 5
+resultado_subtracao = (2 - 10) * 5
+
+# Dividir 27 por 3 e somar 5
+resultado_divisao = (27 / 3) + 5
+
+print(media, resultado_subtracao, resultado_divisao)
+
+modulo = 17 % 4
+print(modulo)
---
+++ Exercicio 6.2.py
@@ -0,0 +1,41 @@
+# 1. Calcule o módulo de 17 por 4.
+modulo = 17 % 4
+print("Módulo de 17 por 4:", modulo)
+
+# 2. Multiplique 9 por 3 e, em seguida, eleve o resultado ao quadrado.
+multiplicacao = 9 * 3
+resultado_quadrado = multiplicacao ** 2
+print("Multiplicação de 9 por 3 e ao quadrado:", resultado_quadrado)
+
+# 3. Calcule a raiz quadrada de 81.
+import math
+raiz_quadrada = math.sqrt(81)
+print("Raiz quadrada de 81:", raiz_quadrada)
+
+# 4. Adicione 20 a 3 vezes 4.
+resultado_adicao = 20 + (3 * 4)
+print("20 + 3 vezes 4:", resultado_adicao)
+
+# 5. Multiplique 15 por 2 e, em seguida, subtraia 7.
+multiplicacao_subtracao = (15 * 2) - 7
+print("Multiplicação de 15 por 2 e subtração de 7:", multiplicacao_subtracao)
+
+# 6. Eleve 5 ao cubo.
+potencia = 5 ** 3
+print("5 elevado ao cubo:", potencia)
+
+# 7. Calcule a média de 17, 21 e 25.
+media = (17 + 21 + 25) / 3
+print("Média de 17, 21 e 25:", media)
+
+# 8. Multiplique 11 por 2 e adicione 7.
+multiplicacao_adicao = (11 * 2) + 7
+print("Multiplicação de 11 por 2 e adição de 7:", multiplicacao_adicao)
+
+# 9. Subtraia 15 de 3 vezes 8 e divida o resultado por 2.
+resultado_divisao = (3 * 8 - 15) / 2
+print("Subtração de 15 de 3 vezes 8 e divisão por 2:", resultado_divisao)
+
+# 10. Eleve 2 à potência de 10.
+potencia_dez = 2 ** 10
+print("2 elevado à potência de 10:", potencia_dez)
