f2633cf80b91c4c5ffb976db3eb44ef5263f2b9f
Ronaldo <rpetronilo@hotmail.com>
2025-03-29 14:53:42 -0300

    Add files via upload

---
+++ exercicio aula 7.py
@@ -0,0 +1,5 @@
+# Exercício 0: Escreva um programa que use a função range() para gerar os números pares de 2 a 20 e, em seguida, imprima cada número.
+# Criando a lista com números ímpares de 1 a 10
+numeros_pares = list(range(2, 21, 2))
+# Imprimindo a lista
+print(numeros_pares)
